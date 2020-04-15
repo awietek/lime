d11a6e90d72aacabfc60aa48657af46dd2f58e8b
awietek <awietek@popeye-login1.cm.cluster>
2020-04-15 10:15:56 -0700

    Added compile options for flatiron gordon

--- options.mk
+++ options.mk
@@ -1,9 +1,18 @@
 ifeq ($(arch), flatiron_linux)
-cc         = clang++
-ccopt      = -O3 -mavx
+cc         = mpicxx
+ccopt      = -O3 -mavx -DLILA_USE_MKL
 ccarch     = -std=c++11 -Wall -pedantic -m64 -Wno-return-type-c-linkage
-libraries  = -L/cm/shared/sw/pkg/devel/hdf5/1.8.21/lib -lhdf5 -lmkl_rt -DLILA_USE_MKL
-liladir    = /mnt/home/awietek/Research/Software/lila
+libraries  = -L/opt/hdf5/gnu/mvapich2_ib/lib -lhdf5 -lmkl_rt -DLILA_USE_MKL
+liladir    = /home/awietek/Research/Software/lila
+includes   = -I. -I$(liladir)
+endif
+
+ifeq ($(arch), flatiron_gordon)
+cc         = mpicxx
+ccopt      = -O3 -mavx -DLILA_USE_MKL
+ccarch     = -std=c++11 -Wall -pedantic -m64 -Wno-return-type-c-linkage
+libraries  = -L/opt/hdf5/gnu/mvapich2_ib/lib -lhdf5 -lmkl_rt -DLILA_USE_MKL
+liladir    = /home/awietek/Research/Software/lila
 includes   = -I. -I$(liladir)
 endif
 
