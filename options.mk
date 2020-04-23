f0151c734de0d682addf1919dbdb271fbef1e548
alexwie <awietek@flatironinstitute.org>
2020-04-23 12:45:05 -0400

    Fixed lila directory for flatiron_linux

--- options.mk
+++ options.mk
@@ -3,7 +3,7 @@ cc         = mpicxx
 ccopt      = -O3 -mavx -DLILA_USE_MKL
 ccarch     = -std=c++11 -Wall -pedantic -m64 -Wno-return-type-c-linkage
 libraries  = -L/opt/hdf5/gnu/mvapich2_ib/lib -lhdf5 -lmkl_rt -DLILA_USE_MKL
-liladir    = /home/awietek/Research/Software/lila
+liladir    = /mnt/home/awietek/Research/Software/lila
 includes   = -I. -I$(liladir)
 endif
 
